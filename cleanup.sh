8b7ba805bd9ab0abd34f2c6743fa5c9bce44345f
evenum <jackherer202@protonmail.com>
2023-12-17 13:45:09 +0300

    Script typechanges

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,20 @@
-/root/bin/cleanup.sh
+#!/bin/bash
+
+year=2019
+
+while [ ${year} -ne 2024 ]
+do
+	if [ -d ${year}-posts ]; then
+		cd "${year}-posts"
+		if [ "$(ls -A .)" ]; then
+			echo "${year}-posts is not empty."
+		else
+			echo "${year}-posts is empty."
+			cd ..
+			rmdir -v "${year}-posts"
+		fi
+	fi
+	((year++))
+done
+
+echo "Successfully removed all empty files."
--- organize.sh
+++ organize.sh
@@ -1,1 +1,18 @@
-/root/bin/organize.sh
+#!/bin/bash
+
+year=2019
+
+while [ ${year} -ne 2024 ]
+do
+	if [ -d ${year}-posts ]; then
+		echo "${year}-posts already exists."
+	else
+		mkdir -v "${year}-posts"
+		echo "Successfully created folder: ${year}-posts."
+	fi
+	mv -i ${year}*.{mp4,jpg,xz,txt} ${year}-posts
+	((year++))
+done
+
+echo "Successfully organized folders+files."
+
